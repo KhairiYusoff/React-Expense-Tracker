3be21fecca2474594b00bd9ffb0798ccf6f5c704
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2022-10-13 10:25:42 +0800

    update

--- src/components/Modal/Modalchild.js
+++ src/components/Modal/Modalchild.js
@@ -1,21 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styles from './Modalchild.module.css'
 
 const Modalchild = (props) => {
+
+    const [isValid, setIsValid] = useState(true)
+
     return (
-        <div style={{
-            display: "flex",
-            flexDirection: "column",
-            padding: "50px",
-            margin: "0 auto",
-            maxWidth: "500px",
-            marginTop: "200px",
-            border: "1px solid black",
-            background: "gray"
-        }}>
-            <button style={{ background: "white", width: "15%", cursor: "pointer" }} onClick={props.onClose}>CLOSE</button>
-            <h1 style={{ textAlign: "center" }}>MODAL</h1>
-            <label htmlFor="">Your Name:</label>
-            <input style={{ marginTop: "20px" }} type="text" placeholder='fullname' />
+        <div className={styles.myDiv} >
+            <button className={styles.myButton} onClick={props.onClose}>CLOSE</button>
+            <h1 className={styles.myHeading}>MODAL</h1>
+            <label className={`${styles['my-label']} ${!isValid && styles.invalid}`} htmlFor="">Your Name:</label>
+            <input className={styles.myInput} type="text" placeholder='fullname' />
         </div>
     )
 }
