67c2cbd39b9a1a24b083c7ce93858cf6ac681ded
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2022-10-25 23:33:10 +0800

    update

--- src/components/Modal/Usereducer.js
+++ src/components/Modal/Usereducer.js
@@ -1,35 +1,20 @@
-import React, { useState } from 'react'
+import React, { useReducer, useState } from "react";
 
-const Usereducer = () => {
+const ACTIONS = {
 
-    const [loading, setLoading] = useState(false)
-    const [post, setPost] = useState({})
-    const [error, setError] = useState(false)
-
-    const handleFetch = () => {
-        setLoading(true)
-        fetch("https://jsonplaceholder.typicode.com/posts/163263")
-            .then(res => res.json())
-            .then((data) => {
-                console.log(data)
-                setPost(data);
-                setLoading(false)
-            })
-            .catch((err) => {
-                setError(true)
-                setLoading(false)
-            })
-    }
-
-    return (
-        <div>
-            <button onClick={handleFetch}>
-                {loading ? "Wait..." : "Fetch the post"}
-            </button>
-            <p>{post?.title}</p>
-            <span>{error && "Something went wrong!"}</span>
-        </div>
-    )
 }
 
-export default Usereducer
+function reducer(state, action) {
+
+}
+
+export default function App() {
+    const [todos, dispatch] = useReducer(reducer, [])
+    const [name, setName] = useState('')
+
+    return (
+        <form>
+            <input type="text" value={name} onChange={e => setName(e.target.value)} />
+        </form>
+    )
+}
