a95de196f3fe7013f5e319cc9509d25a956b73e4
Mohd Khairi Mohd Yusoff <102360814+KhairiYusoff@users.noreply.github.com>
2022-10-31 22:22:16 +0800

    Update

--- src/components/Modal/Parent.js
+++ src/components/Modal/Parent.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Child from './Child'
+import Granchild from './Granchild'
 
 export const NameContext = React.createContext()
 export const AgeContext = React.createContext()
@@ -7,13 +8,26 @@ export const AgeContext = React.createContext()
 const Parent = () => {
 
     const [age, setAge] = useState(37)
+    const [isPara, setIsPara] = useState(false)
+
+    console.log("app running")
+
+    const handleClick = () => {
+        setIsPara(!isPara)
+    }
 
     return (
-        <NameContext.Provider value='khairi'>
-            <AgeContext.Provider value={age}>
-                <Child />
-            </AgeContext.Provider>
-        </NameContext.Provider>
+        <>
+            <NameContext.Provider value='khairi'>
+                <AgeContext.Provider value={age}>
+                    <Child />
+                </AgeContext.Provider>
+            </NameContext.Provider>
+            <h1>React</h1>
+            {isPara && <p>Hi There</p>}
+            <button onClick={handleClick}>Click Me</button>
+        </>
+
     )
 }
 
